405e519610255b4891c379e3e045936ca10ee806
gabriellmandelli <gabriel-mandelli@hotmail.com>
2020-08-19 19:41:05 -0300

    Fix to java 1.8

--- src/main/java/com/softplayer/apply/main/service/validation/PessoaValidator.java
+++ src/main/java/com/softplayer/apply/main/service/validation/PessoaValidator.java
@@ -26,7 +26,7 @@ public class PessoaValidator {
 
     public boolean isPessoaValida(Pessoa pessoa){
 
-        if (pessoa.getNome().isEmpty() || pessoa.getNome().isBlank()){
+        if (pessoa.getNome().isEmpty() || pessoa.getNome().equals(null)){
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Nome obrigatório.");
         }
 
