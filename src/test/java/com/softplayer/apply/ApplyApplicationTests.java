2ccc3dc48bd76ac08f6adb4c94dc418dac6761fb
gabriellmandelli <gabriel-mandelli@hotmail.com>
2020-08-21 02:49:01 -0300

    Teste Docker.

--- src/test/java/com/softplayer/apply/ApplyApplicationTests.java
+++ src/test/java/com/softplayer/apply/ApplyApplicationTests.java
@@ -1,13 +1,8 @@
 package com.softplayer.apply;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplyApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
