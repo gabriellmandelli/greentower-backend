c3b5950f6995b968386e2829d584ca94bd8011fb
gabriellmandelli <gabriel-mandelli@hotmail.com>
2020-10-04 14:32:55 -0300

    Add AuthUser and Session ControllerTest.

--- src/test/java/com/greentower/api/ApiApplicationTests.java
+++ src/test/java/com/greentower/api/ApiApplicationTests.java
@@ -1,8 +1,20 @@
 package com.greentower.api;
 
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest()
 public class ApiApplicationTests {
 
+    @BeforeEach
+    public void login() {
+    }
+
+    @Test
+    public void emptyTest() {
+    }
 }
